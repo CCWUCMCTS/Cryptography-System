04a82c6557cf8f17dc7abf236851909e38b91ef5
CCWUCMCTS <wwwwww931121@163.com>
2020-12-28 22:56:01 +0800

    ecb+cfb

--- BlockCipher/enctypt.py
+++ BlockCipher/enctypt.py
@@ -15,7 +15,7 @@ class Encrypt(CipherBase):
             t = 8 - len(self.data) % 8
             for _ in range(t):
                 self.data += b'\x00'
-    def CBC(self,key,IV):
+    def CBC_E(self,key,IV):
         self.cdata = b''
         a = DES()
         a.generateKey(key)
@@ -25,21 +25,52 @@ class Encrypt(CipherBase):
                 tmp += self.i2b(IV[j]^self.data[i+j],1)
             IV = a.aBlockEncode(tmp)
             self.cdata += IV
-
-    def encryptFile(self,filename,key,IV,mode='cbc',padding='zero',coding='base64'):
+    def ECB_E(self,key):
+        self.cdata = b''
+        a = DES()
+        a.generateKey(key)
+        for i in range(0,len(self.data),8):
+            self.cdata = a.aBlockEncode(self.data[i:i+8])
+    def CFB_E(self,key,IV,cfb_s):
+        if cfb_s % 8 != 0 or cfb_s > 64:
+            raise('明文分组长度超限。')
+        self.cdata = b''
+        a = DES()
+        a.generateKey(key)
+        for i in range(0,len(self.data),cfb_s//8):
+            tmp = b''
+            eIV = a.aBlockEncode(IV)
+            try:
+                for j in range(cfb_s//8):
+                    tmp += self.i2b(eIV[j]^self.data[i+j],1)
+            except:
+                print('没有整除，问题不大。')
+            IV = IV[cfb_s//8:] + tmp
+            self.cdata += tmp
+    def encryptFile(self,filename,key,IV,mode='cbc',padding='zero',coding='base64',cfb_s=64):
         self.readFile(filename)
         self.aPadding(padding)
-        if mode.lower() == 'cbc':
-            self.CBC(key,IV)
+        if mode.lower() == 'ecb':
+            print('请注意，ECB模式可能不安全！')
+            self.ECB_E(key)
+        elif mode.lower() == 'cbc':
+            self.CBC_E(key,IV)
+        elif mode.lower() == 'cfb':
+            self.CFB_E(key,IV,cfb_s)
         else:
             pass
+            
         if coding.lower() == 'base64':
             self.cdata = base64.b64encode(self.cdata)
+            #self.cdata = base64.b64encode(self.cdata.decode('gb2312').encode('utf-8'))
+            
+        elif coding.lower() == 'hex':
+            self.cdata = ''.join([hex(i)[2:] for i in self.cdata]).encode('utf-8')
         else:
-            pass
+            raise('没有这种加密模式！')
         self.writeFile(filename)
 
 if __name__ == '__main__':
     a = Encrypt()
     s = b'zzzzzzzz'
-    a.encryptFile('C:\\Users\\wwwwww931121\\Desktop\\123.txt',s,s)
+    a.encryptFile('C:\\Users\\wwwwww931121\\Desktop\\123.txt',s,s,mode='cfb',padding='zero',coding='base64',cfb_s=8)
